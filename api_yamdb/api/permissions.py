603c8c014367808e842c1152f358be56da57ac4d
Максим Чумаков <maksq077@yandex.ru>
2023-01-18 15:20:07 +0300

    aut

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -1,36 +1,57 @@
 from rest_framework import permissions
 
 
-class IsAdmin(permissions.IsAdminUser):
-    """работа с пользователями."""
+class IsRoleAdmin(permissions.BasePermission):
 
     def has_permission(self, request, view):
+        user = request.user
         return (
-                request.user.is_authenticated
-                and request.user.is_admin
+                user.is_authenticated and user.is_admin
+                or user.is_superuser
+        )
+
+    def has_object_permission(self, request, view, obj):
+        user = request.user
+        return (
+                user.is_authenticated and user.is_admin
+                or user.is_superuser
         )
 
 
-class IsAuthorOrModeratorOrAdminOrReadOnly(
-    permissions.IsAuthenticatedOrReadOnly
-):
-    """для работы с отзывами и комментариями."""
+class IsRoleModerator(permissions.BasePermission):
+
+    def has_permission(self, request, view):
+        user = request.user
+        return (
+                user.is_authenticated and user.is_moderator
+                or user.is_staff
+        )
+
+    def has_object_permission(self, request, view, obj):
+        user = request.user
+        return (
+                user.is_authenticated and user.is_moderator
+                or user.is_staff
+        )
+
+
+class IsAuthorOrReadOnly(permissions.BasePermission):
+
+    def has_permission(self, request, view):
+        user = request.user
+        return (
+                user.is_authenticated and user.is_user
+                or request.method in permissions.SAFE_METHODS
+        )
 
     def has_object_permission(self, request, view, obj):
         return (
-                request.method in permissions.SAFE_METHODS
-                or obj.author == request.user
-                or request.user.is_moderator
-                or request.user.is_admin
+                obj.author == request.user
+                or request.method in permissions.SAFE_METHODS
         )
 
 
-class IsAdminOrReadOnly(permissions.BasePermission):
-    """для работы с категориями и жанрами."""
+class ReadOnly(permissions.BasePermission):
 
     def has_permission(self, request, view):
-        return (
-                request.method in permissions.SAFE_METHODS
-                or request.user.is_authenticated
-                and request.user.is_admin
-        )
+        return request.method in permissions.SAFE_METHODS
---
+++ api_yamdb/api/urls.py
@@ -0,0 +1,14 @@
+from django.urls import include, path
+from rest_framework import routers
+
+from .views import UserViewSet, signup, token, code
+
+router = routers.DefaultRouter()
+router.register('users', UserViewSet, basename='users')
+
+urlpatterns = [
+    path('v1/', include(router.urls)),
+    path('v1/auth/signup/', signup, name='signup'),
+    path('v1/auth/token/', token, name='login'),
+    path('v1/auth/code/', code, name='code'),
+]
