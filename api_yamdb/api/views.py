f98c9ffe37b0290fa134a9e11dcbe224f7f5c7f8
Кристина Новицкая <nko91.yandex.ru>
2023-01-21 20:39:13 +0300

    view_serializer_improvements

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,31 +1,40 @@
-from rest_framework import permissions
-# from api.permissions import IsAuthorOrReadOnly
+from rest_framework.permissions import AllowAny, IsAdminUser
+from api.permissions import IsAuthorOrReadOnly, IsRoleAdmin, IsRoleModerator
 from rest_framework import filters
-from rest_framework.pagination import LimitOffsetPagination
+from api.permissions import IsAuthorOrReadOnly, IsRoleAdmin, IsRoleModerator
 
 from rest_framework import viewsets
 from .serializers import (
     UserSerializer,
     CategorySerializer,
     GenreSerializer,
-    TitleSerializer
+    TitleSerializer,
+    ReviewSerializer,
+    CommentSerializer
 )
-from reviews.models import User, Category, Genre, Title
+from reviews.models import User, Category, Genre, Title, Review, Comment
 
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    permission_classes = [IsRoleAdmin]
 
 
 class CategoryViewSet(viewsets.ModelViewSet):
-    """Viewset для модели Category."""
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
 #    pagination_class = LimitOffsetPagination
     filter_backends = (filters.SearchFilter,)
     search_fields = ('name',)
-#    permission_classes = [IsAuthenticatedOrReadOnly]
+    lookup_field = ('slug')
+
+    def get_permissions(self):
+        if self.action == 'list':
+            permission_classes = [AllowAny]
+        else:
+            permission_classes = [IsRoleAdmin]
+        return [permission() for permission in permission_classes]
 
 
 class GenreViewSet(viewsets.ModelViewSet):
@@ -33,10 +42,40 @@ class GenreViewSet(viewsets.ModelViewSet):
     serializer_class = GenreSerializer
     filter_backends = (filters.SearchFilter,)
     search_fields = ('name',)
-#    permission_classes = [IsAuthenticatedOrReadOnly]
+
+    def get_permissions(self):
+        if self.action == 'list':
+            permission_classes = [AllowAny]
+        else:
+            permission_classes = [IsRoleAdmin]
+        return [permission() for permission in permission_classes]
 
 
 class TitleViewSet(viewsets.ModelViewSet):
     queryset = Title.objects.all()
     serializer_class = TitleSerializer
+
+    def get_permissions(self):
+        if self.action == 'list' or self.action == 'retrieve':
+            permission_classes = [AllowAny]
+        else:
+            permission_classes = [IsRoleAdmin]
+        return [permission() for permission in permission_classes]
+
+
+class ReviewViewSet(viewsets.ModelViewSet):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+
+    def get_permissions(self):
+        if self.action == 'list' or self.action == 'retrieve':
+            permission_classes = [AllowAny]
+        else:
+            permission_classes = [IsRoleAdmin]
+        return [permission() for permission in permission_classes]
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()  # переопределить queryset
+    serializer_class = CommentSerializer
     
