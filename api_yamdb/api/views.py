de9ab4d820ce333e12f4551bccaf1ba88f279307
Григорий Шабалкин <ShadowMoses1314@mail.ru>
2023-01-23 21:07:52 +0600

    Please Check my serializer and views

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -67,20 +67,43 @@ class TitleViewSet(viewsets.ModelViewSet):
 
 
 class ReviewViewSet(viewsets.ModelViewSet):
-    queryset = Review.objects.all()
     serializer_class = ReviewSerializer
+    permission_classes = (IsRoleAdmin, IsRoleModerator, ReadOnly) #может и так можно ?
 
-    def get_permissions(self):
-        if self.action == 'list' or self.action == 'retrieve':
-            permission_classes = [AllowAny]
-        else:
-            permission_classes = [IsRoleAdmin]
-        return [permission() for permission in permission_classes]
+    def get_queryset(self):
+        title_id = self.kwargs.get("title_id")
+        title = get_object_or_404(Title, title_id)
+        new_queryset = title.reviews.all()
+        return new_queryset
+
+    def perform_create(self, serializer):
+        title_id = self.kwargs.get("title_id")
+        title = get_object_or_404(Title, title_id)
+        serializer.save(author=self.request.user, title=title)
+
+#    def get_permissions(self):
+#        if self.action == 'list' or self.action == 'retrieve':
+#            permission_classes = [AllowAny]
+#        else:
+#            permission_classes = [IsRoleAdmin]
+#        return [permission() for permission in permission_classes]
 
 
 class CommentViewSet(viewsets.ModelViewSet):
-    queryset = Comment.objects.all()  # переопределить queryset
     serializer_class = CommentSerializer
+    permission_classes = (IsRoleAdmin, IsRoleModerator, ReadOnly)
+
+    def get_queryset(self):
+        review_id = self.kwargs.get("review_id")
+        review = get_object_or_404(Review, review_id)
+        new_queryset = review.comments.all()
+        return new_queryset
+
+    def perform_create(self, serializer):
+        title_id = self.kwargs.get("title_id")
+        review_id = self.kwargs.get("review_id")
+        review = get_object_or_404(Review, review=review_id, title=title_id)
+        serializer.save(author=self.request.user, review=review)
 
 
 class UserViewSet(viewsets.ModelViewSet):
