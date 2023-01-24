9c2550a698d15579f8d0dbd3e3ec92c7d1e588fd
Григорий Шабалкин <ShadowMoses1314@mail.ru>
2023-01-24 18:45:37 +0600

    Freak AdminSite

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -3,7 +3,14 @@ from django.contrib.auth.admin import UserAdmin
 
 from .models import User, Category, Genre, Title, Review, Comment
 
-admin.site.register(User, UserAdmin)
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('email', 'role', 'bio')
+    search_fields = ('bio',)
+    empty_value_display = '-пусто-'
+
+
 admin.site.register(Title)
 admin.site.register(Genre)
 admin.site.register(Category)
